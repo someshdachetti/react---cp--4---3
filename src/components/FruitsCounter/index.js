ebdad4f25e613bdd86d97c18adc99fc5c08ef085
someshdachetti <dssomesh201@gmail.com>
2023-04-08 05:21:15 +0000

    main

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+
+import {Component} from 'react'
+import './index.css'
+
+class FruitsCounter extends Component {
+  render() {
+    return (
+      <div className="bg-card">
+        <div className="whiteCard">
+          <h>Bob Ate 0 Mango And 0 Banana</h>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
